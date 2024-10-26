82c6f3e12e7cca0ef17c0da9a520dfb6a7d9a194
schadrack10 <schadrackbotombe@gmail.com>
2024-10-26 10:20:23 +0200

    changed context file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ContextProvider from './context/'
+import ContextProvider from './context/contextProvider.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
