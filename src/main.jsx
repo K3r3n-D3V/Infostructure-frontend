fbb905d5b513b989b05666c20218cf163a520992
schadrack10 <schadrackbotombe@gmail.com>
2024-10-26 10:25:09 +0200

    Fix import path for ContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ContextProvider from './context/contextProvider.jsx'
+import ContextProvider from './context/ContextProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
